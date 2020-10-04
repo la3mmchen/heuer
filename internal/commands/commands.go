153f28e051326398f53c06dff229bc28cc2c9d1e
Christoph Petrausch <christoph.petrausch@inovex.de>
2020-10-04 19:35:09 +0200

    Refactor the command into some functions
    The functions reduce the complexity of the over command function.
    Moreover, by reducing the number of nested for loops it is easier to
    reason about the code.
    
    In general, we check in go code the error condition first. In case of an
    error abort the loop or function.

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -2,12 +2,12 @@ package commands
 
 import (
 	"fmt"
+	"github.com/xeonx/timeago"
 	"log"
 
 	"github.com/adlio/trello"
 	"github.com/la3mmchen/treta/internal/types"
 	"github.com/urfave/cli"
-	"github.com/xeonx/timeago"
 )
 
 const (
@@ -41,74 +41,96 @@ func read(cfg types.Configuration) cli.Command {
 		// client object to interact with trello
 		client := trello.NewClient(cfg.TrelloAppKey, cfg.TrelloToken)
 
-		member, err := client.GetMember(cfg.TrelloUserName, trello.Defaults())
+		wanted := types.NewWantList(cfg.TrelloList)
+		lists, err := getListsFromBoard(client, cfg)
 		if err != nil {
 			log.Fatal(err)
 		}
-		boards, err := member.GetBoards(trello.Defaults())
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		for _, v := range boards {
-			if v.Name == cfg.TrelloBoard {
-
-				board, err := client.GetBoard(v.ID, trello.Defaults())
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				lists, err := board.GetLists(trello.Defaults())
-				if err != nil {
-					log.Fatal(err)
-				}
-				var found bool = false
-				for _, x := range lists {
-					for _, wantedList := range cfg.TrelloList {
-						if x.Name == wantedList {
-							found = true
-							fmt.Printf("cards in [%v]: \n", wantedList)
-							list, err := client.GetList(x.ID, trello.Defaults())
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							cards, err := list.GetCards(trello.Defaults())
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							for _, y := range cards {
-								var out string
-								if y.DueComplete {
-									out += "[done] "
-								} else {
-									out += "[    ] "
-								}
-								out += y.Name
-
-								if y.Due != nil {
-									out += "\n" + indent + "Due: " + timeago.English.Format(*y.Due)
-								}
-
-								if len(y.Desc) > 0 {
-									out += "\n" + indent + "Description: " + y.Desc
-								}
-								fmt.Printf("%v \n", out)
-								fmt.Printf("\n")
-								out = " "
-
-							}
-							fmt.Printf("___ \n")
-						}
-					}
-				}
-				if !found {
-					fmt.Printf("Sorry, the wanted lists are not found in your trello account.. \n")
-				}
-			}
+		found := renderLists(client, lists, wanted)
+		if !found {
+			fmt.Printf("Sorry, the wanted lists are not found in your trello account.. \n")
 		}
 		return nil
 	}
 	return cmd
+}
+
+// getListsFromBoard returns the lists of the specified trello board
+func getListsFromBoard(client *trello.Client, cfg types.Configuration) ([]*trello.List, error) {
+	member, err := client.GetMember(cfg.TrelloUserName, trello.Defaults())
+	if err != nil {
+		// Use the %w in the format string. This is a new feature introduced in go 1.13 to wrap errors.
+		// See https://blog.golang.org/go1.13-errors for details
+		return nil, fmt.Errorf("failed to get trello member struct: %w", err)
+	}
+
+	boards, err := member.GetBoards(trello.Defaults())
+	if err != nil {
+		return nil, fmt.Errorf("failed to list all trello boards: %w", err)
+	}
+	var boardID string
+	for _, v := range boards {
+		if v.Name == cfg.TrelloBoard {
+			boardID = v.ID
+			break
+		}
+	}
+	if boardID == "" {
+		return nil, fmt.Errorf("did not find the specified trello board in your account")
+	}
+	board, err := client.GetBoard(boardID, trello.Defaults())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get board details: %w ", err)
+	}
+
+	lists, err := board.GetLists(trello.Defaults())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get lists of the specified trello board: %w", err)
+	}
+	return lists, nil
+}
+
+// renderLists renders all wanted trello lists to stdout
+func renderLists(client *trello.Client, lists []*trello.List, wanted types.WantList) bool {
+	var found bool = false
+	for _, x := range lists {
+		if wanted.IsWanted(x.Name) {
+			found = true
+			fmt.Printf("cards in [%v]: \n", x.Name)
+			list, err := client.GetList(x.ID, trello.Defaults())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			cards, err := list.GetCards(trello.Defaults())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, y := range cards {
+				var out string
+				if y.DueComplete {
+					out += "[done] "
+				} else {
+					out += "[    ] "
+				}
+				out += y.Name
+
+				if y.Due != nil {
+					out += "\n" + indent + "Due: " + timeago.English.Format(*y.Due)
+				}
+
+				if len(y.Desc) > 0 {
+					out += "\n" + indent + "Description: " + y.Desc
+				}
+				fmt.Printf("%v \n", out)
+				fmt.Printf("\n")
+				out = " "
+
+			}
+			fmt.Printf("___ \n")
+		}
+	}
+	return found
 }
--- internal/types/structs.go
+++ internal/types/structs.go
@@ -12,3 +12,24 @@ type Configuration struct {
 	TrelloList     []string
 	Debug          string
 }
+
+// WantList is a mapping from string to empty struct to describe the set of wanted strings.
+// We use an empty struct instead of a boolean since this saves us 1 byte of memory per value :-D
+// https://play.golang.org/p/fc14T5vr_xS
+type WantList map[string]struct{}
+
+func NewWantList(values []string) WantList {
+	w := make(WantList, len(values))
+	for _, s := range values {
+		w[s] = struct{}{}
+	}
+	return w
+}
+
+// IsWanted returns true if the given string is in the WantList
+// We use the multi return value form of getting a value from a dict
+// The second return value is a boolean indicating if the key is in the map.
+func (w WantList) IsWanted(s string) bool {
+	_, found := w[s]
+	return found
+}
